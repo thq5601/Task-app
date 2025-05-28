67580ac5e1f4c7af267f5170aa987ec8e9fc2713
QuanTH <thq5601@gmail.com>
2025-05-28 10:15:52 +0700

    Initial commit

---
+++ frontend/lib/features/auth/pages/signup_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign Up.',
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              TextFormField()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
