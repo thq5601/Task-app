161cf36cdaf9a4f93aea777bd065deaf735ac250
QuanTH <thq5601@gmail.com>
2025-05-29 13:18:46 +0700

    Create init for backend

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Test API is working?')
+})
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000')
+})
