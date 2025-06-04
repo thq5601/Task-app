3288ae5fc63083d9cb4792ebfd47c2e5b36a5712
QuanTH <thq5601@gmail.com>
2025-06-04 11:56:27 +0700

    Refactor SignupPage to integrate BlocConsumer for authentication state management

--- frontend/lib/features/auth/pages/signup_page.dart
+++ frontend/lib/features/auth/pages/signup_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/features/auth/cubit/auth_cubit.dart';
 import 'package:frontend/features/auth/pages/login_page.dart';
 import 'package:frontend/widget/reusable_button.dart';
 import 'package:frontend/widget/reusable_textform.dart';
@@ -20,9 +22,12 @@ class _SignupPageState extends State<SignupPage> {
 
   void _signupValidate() {
     if (_formKey.currentState!.validate()) {
-      //
+      context.read<AuthCubit>().signUp(
+        name: nameController.text.trim(),
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
     }
-    ;
   }
 
   @override
@@ -36,126 +41,149 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Sign Up.',
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 32),
-
-                // Name field
-                ReusableTextForm(
-                  hintText: 'Full Name',
-                  controller: nameController,
-                  prefixIcon: Icons.person,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Full name is required';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-
-                // Email field
-                ReusableTextForm(
-                  hintText: 'Email',
-                  controller: emailController,
-                  prefixIcon: Icons.email,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email address';
-                    }
-
-                    String emailPattern =
-                        r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
-                    RegExp regExp = RegExp(emailPattern);
-
-                    if (!regExp.hasMatch(value)) {
-                      return 'Please enter a valid email address';
-                    }
-
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                ReusableTextForm(
-                  hintText: 'Password',
-                  controller: passwordController,
-                  isPassword: true,
-                  prefixIcon: Icons.lock,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Password is required';
-                    }
-
-                    if (value.length < 8) {
-                      return 'Password must be at least 8 characters long';
-                    }
-
-                    if (value.length > 32) {
-                      return 'Password must be less than 32 characters';
-                    }
-
-                    // Check for at least one uppercase letter
-                    if (!RegExp(r'[A-Z]').hasMatch(value)) {
-                      return 'Password must contain at least one uppercase letter';
-                    }
-
-                    // Check for at least one lowercase letter
-                    if (!RegExp(r'[a-z]').hasMatch(value)) {
-                      return 'Password must contain at least one lowercase letter';
-                    }
-
-                    // Check for at least one digit
-                    if (!RegExp(r'[0-9]').hasMatch(value)) {
-                      return 'Password must contain at least one number';
-                    }
-
-                    // Check for at least one special character
-                    if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(value)) {
-                      return 'Password must contain at least one special character';
-                    }
-
-                    return null;
-                  },
-                ),
-
-                const SizedBox(height: 16),
-                ReusableButton(
-                  text: 'Sign Up',
-                  backgroundColor: Colors.black,
-                  onPressed: _signupValidate,
-                ),
-                const SizedBox(height: 16),
-                InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(LoginPage.route());
-                  },
-                  child: RichText(
-                    text: TextSpan(
-                      style: Theme.of(context).textTheme.titleMedium,
-                      text: 'Already have an account? ',
-                      children: [
-                        const TextSpan(
-                          text: 'Login',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ],
+      body: BlocConsumer<AuthCubit, AuthState>(
+        listener: (context, state) {
+          if (state is AuthError) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.error)));
+          } else if (state is AuthSignUp) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Account created! Login NOW!")),
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Sign Up.',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
+                    const SizedBox(height: 32),
+
+                    // Name field
+                    ReusableTextForm(
+                      hintText: 'Full Name',
+                      controller: nameController,
+                      prefixIcon: Icons.person,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Full name is required';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+
+                    // Email field
+                    ReusableTextForm(
+                      hintText: 'Email',
+                      controller: emailController,
+                      prefixIcon: Icons.email,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email address';
+                        }
+
+                        String emailPattern =
+                            r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+                        RegExp regExp = RegExp(emailPattern);
+
+                        if (!regExp.hasMatch(value)) {
+                          return 'Please enter a valid email address';
+                        }
+
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    ReusableTextForm(
+                      hintText: 'Password',
+                      controller: passwordController,
+                      isPassword: true,
+                      prefixIcon: Icons.lock,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Password is required';
+                        }
+
+                        if (value.length < 8) {
+                          return 'Password must be at least 8 characters long';
+                        }
+
+                        if (value.length > 32) {
+                          return 'Password must be less than 32 characters';
+                        }
+
+                        // Check for at least one uppercase letter
+                        if (!RegExp(r'[A-Z]').hasMatch(value)) {
+                          return 'Password must contain at least one uppercase letter';
+                        }
+
+                        // Check for at least one lowercase letter
+                        if (!RegExp(r'[a-z]').hasMatch(value)) {
+                          return 'Password must contain at least one lowercase letter';
+                        }
+
+                        // Check for at least one digit
+                        if (!RegExp(r'[0-9]').hasMatch(value)) {
+                          return 'Password must contain at least one number';
+                        }
+
+                        // Check for at least one special character
+                        if (!RegExp(
+                          r'[!@#$%^&*(),.?":{}|<>]',
+                        ).hasMatch(value)) {
+                          return 'Password must contain at least one special character';
+                        }
+
+                        return null;
+                      },
+                    ),
+
+                    const SizedBox(height: 16),
+                    ReusableButton(
+                      text: 'Sign Up',
+                      backgroundColor: Colors.black,
+                      onPressed: _signupValidate,
+                    ),
+                    const SizedBox(height: 16),
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(LoginPage.route());
+                      },
+                      child: RichText(
+                        text: TextSpan(
+                          style: Theme.of(context).textTheme.titleMedium,
+                          text: 'Already have an account? ',
+                          children: [
+                            const TextSpan(
+                              text: 'Login',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
