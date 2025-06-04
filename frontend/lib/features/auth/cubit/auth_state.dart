d692723f760230189a77297c394574a55d2c126b
QuanTH <thq5601@gmail.com>
2025-06-04 11:55:56 +0700

    Fix formatting and reorder AuthError class in auth_state.dart

--- frontend/lib/features/auth/cubit/auth_state.dart
+++ frontend/lib/features/auth/cubit/auth_state.dart
@@ -1,4 +1,4 @@
-part of 'auth_cubit.dart';
+part of "auth_cubit.dart";
 
 sealed class AuthState {}
 
@@ -8,12 +8,12 @@ final class AuthLoading extends AuthState {}
 
 final class AuthSignUp extends AuthState {}
 
-final class AuthError extends AuthState {
-  final String error;
-  AuthError(this.error);
-}
-
 final class AuthLoggedIn extends AuthState {
   final UserModel user;
   AuthLoggedIn(this.user);
+}
+
+final class AuthError extends AuthState {
+  final String error;
+  AuthError(this.error);
 }
