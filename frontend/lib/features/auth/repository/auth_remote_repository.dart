9f439d1d43f6df6651801f1e6878255724855a20
QuanTH <thq5601@gmail.com>
2025-06-05 10:52:01 +0700

    Add getUserData method to AuthRemoteRepository for user data retrieval

--- frontend/lib/features/auth/repository/auth_remote_repository.dart
+++ frontend/lib/features/auth/repository/auth_remote_repository.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 
 import 'package:frontend/core/constants/constants.dart';
+import 'package:frontend/core/services/sp_service.dart';
 import 'package:frontend/models/user_model.dart';
 import 'package:http/http.dart' as http;
 
 class AuthRemoteRepository {
+  final spService = SharedPrefService();
+
   Future<UserModel> signUp({
     required String name,
     required String email,
@@ -45,6 +48,42 @@ class AuthRemoteRepository {
       return UserModel.fromJson(res.body);
     } catch (e) {
       throw e.toString();
+    }
+  }
+
+  Future<UserModel?> getUserData() async {
+    try {
+      final token = await spService.getToken();
+
+      if (token == null) {
+        return null;
+      }
+
+      final res = await http.post(
+        Uri.parse('${Constants.backendUri}/auth/tokenIsValid'),
+        headers: {'Content-Type': 'application/json', 'x-auth-token': token},
+      );
+
+      
+
+      if (res.statusCode != 200 || jsonDecode(res.body) == false) {
+        return null;
+      }
+
+      final userResponse = await http.get(
+        Uri.parse('${Constants.backendUri}/auth'),
+        headers: {'Content-Type': 'application/json', 'x-auth-token': token},
+      );
+      
+
+      if (userResponse.statusCode != 200) {
+        throw jsonDecode(userResponse.body)['error'];
+      }
+
+      return UserModel.fromJson(userResponse.body);
+    } catch (e) {
+      print(e);
+      return null;
     }
   }
 }
