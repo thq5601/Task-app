c22ca6069ec197b8289ad485089b0b6a19b5c86d
QuanTH <thq5601@gmail.com>
2025-06-05 10:51:43 +0700

    Refactor main.dart to implement MultiBlocProvider and initialize AuthCubit in MyApp

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,29 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/features/auth/cubit/auth_cubit.dart';
-import 'package:frontend/features/auth/pages/login_page.dart';
+import 'package:frontend/features/auth/pages/signup_page.dart';
+import 'package:frontend/features/home/page/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => AuthCubit())],
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    context.read<AuthCubit>().getUserData();
+    super.initState();
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (_)=> AuthCubit()),
-      ],
-      child: MaterialApp(
-        title: 'Task App',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: LoginPage(),
+    return MaterialApp(
+      title: 'Task App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: BlocBuilder<AuthCubit, AuthState>(
+        builder: (context, state) {
+          if (state is AuthLoggedIn) {
+            return const HomePage();
+          }
+          return SignupPage();
+        },
       ),
     );
   }
